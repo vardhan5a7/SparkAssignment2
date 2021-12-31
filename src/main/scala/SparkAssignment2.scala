22297bd113f9cba08cf7b5d67ead2cdf1f974389
vardhan5a7 <66350489+vardhan5a7@users.noreply.github.com>
2021-12-31 12:08:56 +0530

    Update SparkAssignment2.scala

--- src/main/scala/SparkAssignment2.scala
+++ src/main/scala/SparkAssignment2.scala
@@ -73,7 +73,7 @@ object SparkAssignment2 extends App{
     val reqfield = logsData.flatMap(x => x.split(","))
       .filter(x => x.contains("ghtorrent.rb: Repo") && x.contains("exists"))
       .map(x => (x.substring(x.indexOf("Repo")+5,x.indexOf("exists")-1),1))
-      .reduceByKey((x,y) => x+y).sortBy(x => x._2,false).take(10)
+      .reduceByKey((x,y) => x+y).sortBy(x => x._2,false).take(20)
       .foreach(println)
   }
 
